898b6ee9520f0657279e5068b3488a2312dffa5e
Aditya Yadav <110929760+pennyvice@users.noreply.github.com>
2023-08-26 16:58:17 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main() {
     int n, factorial = 1;
-
+   
     cout << "Enter a positive integer: ";
     cin >> n;
-
+ if(n<0){
+        cout<<"Negative number";
+        return 1;
+    }
     for(int i = 1; i <= n; ++i) {
         factorial *= i;
     }
