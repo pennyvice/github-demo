5ca6c8ea26adac7d268621ca6dc562ba115fa3e4
Aditya <adityayadav737.ay@gmail.com>
2023-08-26 16:47:48 +0530

    Error correction related to negative numbers

--- factorial.cpp
+++ factorial.cpp
@@ -6,7 +6,10 @@ int main() {
 
     cout << "Enter a positive integer: ";
     cin >> n;
-
+	if(n<0){
+		cout<<"Wrong Input";
+		return 1;
+	}
     for(int i = 1; i <= n; ++i) {
         factorial *= i;
     }
